38e089bf6f8b1c06018747d466c533f5fc8258dd
Jose Mosquera-Linux <jojumofu@gmail.com>
2023-03-03 11:12:10 -0500

    create: portfolio models

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
-# Create your models here.
+from django.db.models.fields import CharField, URLField
+from django.db.models.fields.files import ImageField
+class Project(models.Model):
+    title = CharField(max_length=100)
+    description = CharField(max_length=250)
+    image = ImageField(upload_to='portfolio/images/')
+    url = UrlField(blank=True)
